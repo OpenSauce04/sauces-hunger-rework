92557f58c0071ed3fee89e0753cadc5f61ab0d39
OpenSauce04 <opensauce04@gmail.com>
2022-11-24 21:24:28 +0000

    Bumped version number to 1.0.1

--- src/main/java/saucesfoodrework/SaucesFoodRework.java
+++ src/main/java/saucesfoodrework/SaucesFoodRework.java
@@ -16,7 +16,7 @@ public class SaucesFoodRework
   //  build.gradle file (the version, group, and archivesBaseName parameters)
   //  resources/mcmod.info (the name, description, and version parameters)
   public static final String MODID = "saucesfoodrework";
-  public static final String VERSION = "1.0";
+  public static final String VERSION = "1.0.1";
 
 	@EventHandler
 	public void init(FMLInitializationEvent event)
