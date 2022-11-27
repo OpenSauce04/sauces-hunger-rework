2e53fea314ddf39b78c8d0b4b423c4e2eb3175d6
OpenSauce04 <opensauce04@gmail.com>
2022-11-27 13:27:20 +0000

    Fixed mod not correctly detecting when the player runs out of hunger

--- src/main/java/sauceshungerrework/ExhaustionEventHandler.java
+++ src/main/java/sauceshungerrework/ExhaustionEventHandler.java
@@ -5,24 +5,35 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.player.EntityPlayer;
 import squeek.applecore.api.AppleCoreAPI;
 import squeek.applecore.api.hunger.ExhaustionEvent;
 
-public class ExhaustionEventHandler {
-  @SubscribeEvent
-	public void onExhaustionAddition(ExhaustionEvent.ExhaustionAddition event) {
 
-		if (event.player.getFoodStats().getSaturationLevel() > 0f) { // 
-      AppleCoreAPI.mutator.setHunger(event.player, 20); // Allow healing, disable eating food
+public class ExhaustionEventHandler {
+
+  private void HandleExhaustion(EntityPlayer player, float deltaSaturation) {
+
+    if (player.getFoodStats().getSaturationLevel() + deltaSaturation > 0f) { 
+      AppleCoreAPI.mutator.setHunger(player, 20); // Allow healing, disable eating food
     } else {
-      AppleCoreAPI.mutator.setHunger(event.player, 17); // Just enough to not allow natural health recovery
-      AppleCoreAPI.mutator.setExhaustion(event.player, 0);
+      AppleCoreAPI.mutator.setHunger(player, 17); // Just enough to not allow natural health recovery
+      AppleCoreAPI.mutator.setExhaustion(player, 0);
     }
 
   }
 
   @SubscribeEvent
-	public void onExhaustionAction(ExhaustionEvent.ExhaustingAction event) {
+	public void onExhaustionAddition(ExhaustionEvent.ExhaustionAddition event) {
+    HandleExhaustion(event.player, 0f);
+  }
+  @SubscribeEvent
+	public void onExhausted(ExhaustionEvent.Exhausted event) {
+    HandleExhaustion(event.player, event.deltaSaturation);
+  }
+
+  @SubscribeEvent
+	public void onExhaustingAction(ExhaustionEvent.ExhaustingAction event) {
     event.deltaExhaustion = 0f;
   }
   
